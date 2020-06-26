11ae0c0f4c2705e844a2a41005df2e8a0d3dfce3
msteinwert <mlsteinwert@ucdavis.edu>
2020-06-26 11:10:03 -0700

    testing how version control works

--- web_downloader_run.py
+++ web_downloader_run.py
@@ -4,3 +4,6 @@ if __name__ == '__main__':
     url = 'https://ucdavis.app.box.com/folder/68864389765'
     geturl = requests.get(url, allow_redirects=True)
     print(geturl.headers.get('content-type'))
+
+
+# Test version control
