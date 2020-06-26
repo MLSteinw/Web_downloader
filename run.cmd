f34dc24ae04d0c179ff4ceadd79fbd00b4057dc7
msteinwert <mlsteinwert@ucdavis.edu>
2020-06-26 12:57:08 -0700

    testing to see if can run from venv

---
+++ run.cmd
@@ -0,0 +1,1 @@
+"%~dp0venv\Scripts\python.exe" "%~dp0web_downloader_run.py"  pause
--- web_downloader_run.py
+++ web_downloader_run.py
@@ -1,9 +1,11 @@
 import requests
 
 if __name__ == '__main__':
-    url = 'https://ucdavis.app.box.com/folder/68864389765'
-    geturl = requests.get(url, allow_redirects=True)
-    print(geturl.headers.get('content-type'))
+    print('hello')
+    x = input("press any key to continue")
+    # url = 'https://ucdavis.app.box.com/folder/68864389765'
+    # geturl = requests.get(url, allow_redirects=True)
+    # print(geturl.headers.get('content-type'))
 
 
 # Test version control
